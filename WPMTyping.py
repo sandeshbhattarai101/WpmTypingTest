6bb063db6a8935dd6db7bb4be7a848e5e729b05d
sandesh2057 <sandeshbhattarai6@gmail.com>
2025-06-19 17:29:04 +0545

    first commit

---
+++ WPMTyping.py
@@ -0,0 +1,76 @@
+import curses
+from curses import wrapper
+import time
+
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Tying Test!")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+
+        stdscr.addstr(0, i, char, color)
+
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app!"
+    current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True) 
+    # stdscr.nodelay() avoids blocking statement such as input and below key
+    # statement and if user delays throws the exception which can be handled using try except
+
+
+
+    while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round(len(current_text) / (time_elapsed / 60 ) / 5)
+
+
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+        
+        
+        # TO EXIT THE TERMINAL ord() finds ascii value of a key and 27 is ascii for esc key
+        if ord(key) == 27:
+            break
+
+        if key in ("KEY_BACKSPACE", '\b', '\x7f'):
+            if len(current_text) > 0:
+                current_text.pop()
+
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
+
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
+    time.sleep(5)
+
+wrapper(main)
+
